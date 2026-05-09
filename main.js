dfafd3e5ff15b9914f8e4046d2487510ec56fa7d
Dwi Andhika <53227252+dwiandhikaap@users.noreply.github.com>
2026-05-09 21:11:45 +0700

    test

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("asdddddddddddd")
+registerPlugin(
+    {
+        "left": {
+            kind: "number"
+        },
+        "right": {
+            kind: "number"
+        }
+    },
+    {
+        "result": {
+            kind: "number"
+        }
+    },
+    async (input) => {
+        output.result = input.left + input.right;
+
+        return {
+            result: input.left + input.right
+        };
+    }
+);
